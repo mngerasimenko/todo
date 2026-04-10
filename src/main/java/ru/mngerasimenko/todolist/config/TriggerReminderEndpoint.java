e8c151e62c68e06a8f18fd8ae8d495e2b7046ec1
mngerasimenko <mngerasimenko@gmail.com>
2026-04-10 12:15:41 +0400

    feat: add /actuator/triggerreminder endpoint for manual reminder testing

---
+++ src/main/java/ru/mngerasimenko/todolist/config/TriggerReminderEndpoint.java
@@ -0,0 +1,27 @@
+package ru.mngerasimenko.todolist.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+import ru.mngerasimenko.todolist.scheduler.InactiveReminderScheduler;
+
+import java.util.Map;
+
+/**
+ * Actuator-эндпоинт для ручного запуска напоминаний неактивным пользователям.
+ * Доступен только на management-порту: http://localhost:8091/actuator/triggerreminder
+ */
+@Component
+@Endpoint(id = "triggerreminder")
+@RequiredArgsConstructor
+public class TriggerReminderEndpoint {
+
+    private final InactiveReminderScheduler scheduler;
+
+    @ReadOperation
+    public Map<String, String> trigger() {
+        scheduler.sendReminders();
+        return Map.of("status", "done", "message", "Inactive reminder triggered");
+    }
+}
